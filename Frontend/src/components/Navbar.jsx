1d7f543c5bc4fdc066d6cde20fd89167e3546934
Hamza Mubin <hamzamubeen182@gmail.com>
2025-08-06 14:34:53 +0530

    finalizing project fe7

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.scss';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>🧠 PanScience Task Manager</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import './styles/main.scss';
+import Navbar from './components/Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
+      <Navbar />
       <App />
     </AuthProvider>
   </BrowserRouter>
